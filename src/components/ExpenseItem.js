bf19f5c37615286f673608045ab2ae2699b0dce3
DesperateHiking <perevechenkov_1999@mail.ru>
2022-01-30 04:44:30 +0500

    passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 // import React from 'react';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <div>Dec 6th 2021</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Forza Horizon 5</h2>
-        <div className='expense-item__price'>100$</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>{props.amount}$</div>
       </div>
     </div>
   );
